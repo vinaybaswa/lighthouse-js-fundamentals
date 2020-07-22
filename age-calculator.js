04be462a466a9298b70ae04111c32d9fc6e238d6
Vinay Baswa <vinaybaswa@gmail.com>
2020-07-22 03:22:32 +0000

    calculate a person's age

---
+++ age-calculator.js
@@ -0,0 +1,5 @@
+function ageCalculator (name, yearOfBirth, currentYear) {
+  return (name + " is " + (currentYear - yearOfBirth) + " years old.");
+}
+
+console.log(ageCalculator("Navya", 1987, 2020));
