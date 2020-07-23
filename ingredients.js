b43af88f21da74addf5048bec4d3c4c1c4048cde
Vinay Baswa <vinaybaswa@gmail.com>
2020-07-23 14:22:50 +0000

    print ingredients of banana cake

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (j = 0; j < ingredients.length; j++) {
+  console.log(ingredients[j]);
+}
+
+let k = ingredients.length - 1;
+while (k >=0 ) {
+  console.log(ingredients[k]);
+  k--;
+}
