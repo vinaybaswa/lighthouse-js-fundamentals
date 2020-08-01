6e2d70d74a0aeb73942e09abe9c2249960f66f7f
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-01 00:13:59 +0000

    updated with Math.max

--- kata-1.js
+++ kata-1.js
@@ -1,13 +1,14 @@
 const sumLargestNumbers = function (data) {
   const largestTwo = [];
-  const max1 = data.reduce(function (a, b) { return Math.max(a, b); });
+  const max1 = Math.max(...data);
   largestTwo.push(max1);
   data.splice(data.indexOf(max1), 1);
-  const max2 = data.reduce(function (a, b) { return Math.max(a, b); });
+  const max2 = Math.max(...data);
   largestTwo.push(max2);
   return largestTwo.reduce(function (a, b) { return a + b; });
 };
 
+// Test 
 console.log(sumLargestNumbers([1, 10]));
 console.log(sumLargestNumbers([1, 2, 3]));
 console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
