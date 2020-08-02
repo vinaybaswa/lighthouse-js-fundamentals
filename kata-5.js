9de053f5725b08eb78758ce6be2fc71e1d6fb720
VinBaz <67991028+vinaybaswa@users.noreply.github.com>
2020-08-02 00:19:58 -0400

    Delete kata-5.js

--- kata-5.js
+++
@@ -1,9 +0,0 @@
-const urlEncode = function (text) {
-  let newText = text.trim();
-  return newText.replace(/\s/g, "%20")
-};
-
-
-console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode(" Lighthouse Labs "));
-console.log(urlEncode("blue is greener than purple for sure"));
